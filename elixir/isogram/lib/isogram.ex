8dd445b30801ad6c410ceb836a859ef0a6e42a42
Jamie Curle <jamie@curle.io>
2026-05-22 09:07:32 +0100

    improved: changed isogram based on feedback

--- elixir/isogram/lib/isogram.ex
+++ elixir/isogram/lib/isogram.ex
@@ -2,21 +2,23 @@ defmodule Isogram do
   @doc """
   Determines if a word or sentence is an isogram
   """
-  @spec isogram?(String.t()) :: boolean
-  def isogram?(sentence) do
+  @spec isogram?(String.t() | list) :: boolean
+  def isogram?(sentence) when is_binary(sentence) do
     sentence
     |> String.downcase()
     |> String.replace(~r/[^a-z]/, "")
     |> String.graphemes()
-    |> is_isogram?()
+    |> isogram?()
   end
 
-  defp is_isogram?([]), do: true
+  def isogram?([] = graphemes) when is_list(graphemes), do: true
 
-  defp is_isogram?(graphemes) do
+  def isogram?(graphemes) when is_list(graphemes) do
     graphemes
     |> Enum.frequencies()
     |> Map.values()
     |> Enum.all?(&(&1 <= 1))
   end
+
+  def isogram?(_), do: false
 end
